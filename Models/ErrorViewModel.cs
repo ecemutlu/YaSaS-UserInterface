bee5e1c774533f696cb2a94017fd72f7c0fdd04f
ecemutlu <mutlu.ece@hotmail.com.tr>
2024-04-21 23:58:38 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YaSaS_UserInterface.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
