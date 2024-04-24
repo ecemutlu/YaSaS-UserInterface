39505405727ae1eea5d09f4b6b04bf7e0481116c
ecemutlu <mutlu.ece@hotmail.com.tr>
2024-04-24 13:13:16 +0300

    Login controller added.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YaSaS_UserInterface.Controllers
+{
+	public class LoginController : Controller
+	{
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+		//[HttpPost]
+		//public async Task<IActionResult> Login()
+		//{
+		//	return View();
+		//}
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+		//[HttpPost]
+		//public IActionResult SignIn()
+		//{
+		//	return View();
+		//}
+	}
+}
+
